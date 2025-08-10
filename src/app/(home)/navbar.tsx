1472724644c26d99d367f3def541450c96ccba08
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-08-11 01:23:22 +0530

    new

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -5,6 +5,9 @@ import { cn } from "@/lib/utils"
 import { Poppins } from "next/font/google"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useState } from "react";
+import { NavbarSidebar } from "./navbar-sidebar";
+import { MenuIcon } from "lucide-react";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -40,16 +43,19 @@ const NavbarItem = ({
 
 
 const navbarItems = [
-    { href: "/" , Children: "Home" },
-    { href: "/about" , Children: "About" },
-    { href: "/features" , Children: "Features" },
-    { href: "/pricing" , Children: "Pricing" },
-    { href: "/contact" , Children: "Contact" },
-]
+    { href: "/" , children: "Home" },
+    { href: "/about" , children: "About" },
+    { href: "/features" , children: "Features" },
+    { href: "/pricing" , children: "Pricing" },
+    { href: "/contact" , children: "Contact" },
+];
 
 
 export const Navbar = () => {
     const pathname = usePathname();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+
 
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
@@ -58,6 +64,12 @@ export const Navbar = () => {
                     funroad
                 </span>
             </Link>
+            
+            <NavbarSidebar 
+                items={navbarItems}
+                open={isSidebarOpen}
+                onOpenChange={setIsSidebarOpen}
+            />
 
             <div className="items-center gap-4 hidden lg:flex ">
                 {navbarItems.map((item) => (
@@ -66,9 +78,36 @@ export const Navbar = () => {
                         href={item.href}
                         isActive={pathname === item.href}
                     >
-                        {item.Children}
+                        {item.children}
                     </NavbarItem>
                 ))}
+            </div>
+            <div className="hidden lg:flex">
+                <Button
+                    variant="secondary"
+                    className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+                >
+                    <Link href="/sign-in" >
+                        Log in
+                    </Link>
+                </Button>
+                <Button 
+                    variant="secondary"
+                    className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg "
+                >
+                    <Link href="/sign-up" >
+                        Start selling
+                    </Link>
+                </Button>
+            </div>
+            <div className="flex lg:hidden items-center justify-center" >
+                <Button
+                    variant="ghost"
+                    className=" size-12 border-transparent bg-white "
+                    onClick={() => setIsSidebarOpen(true)}
+                >
+                    <MenuIcon />
+                </Button>
             </div>
         </nav>
     )
